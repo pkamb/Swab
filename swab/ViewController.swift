31f80903126b75b1267372f332e9d66a27c5097e
Peter Kamb <peterkamb@gmail.com>
2015-09-19 10:49:11 -0700

    Initial commit of Xcode new Single View iOS Swift project.

---
+++ swab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swab
+//
+//  Created by Peter Kamb on 9/19/15.
+//  Copyright © 2015 Knurling Group, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
