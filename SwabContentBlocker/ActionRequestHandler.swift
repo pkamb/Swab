a2ae36f1ade6bc12218bdebabfbc62a47f257a09
Peter Kamb <peterkamb@gmail.com>
2015-09-19 10:56:41 -0700

    Add new Target, iOS Content Blocker Extension

---
+++ SwabContentBlocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  SwabContentBlocker
+//
+//  Created by Peter Kamb on 9/19/15.
+//  Copyright © 2015 Knurling Group, LLC. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
