41dc20c60a561926e0fbe2cfb5c7db6cc3558e8a
Peter Kamb <peterkamb@gmail.com>
2016-09-10 10:38:54 -0700

    Swift 3 conversion via Xcode 8 GM

--- swab/SwabSettings.swift
+++ swab/SwabSettings.swift
@@ -12,26 +12,26 @@ class SwabSettings: UITableViewController {
     
     @IBOutlet weak var headerText: UITextView?
     
-    private enum Section: Int {
-        case Installation
-        case Settings
-        case Links
-        case Swab
-        case WellVetted
-        case OneBlock
-        case CostPerBlock
-        case InTheDetails
-        case Pricing
-        case DareYou
-        case Contact
+    fileprivate enum Section: Int {
+        case installation
+        case settings
+        case links
+        case swab
+        case wellVetted
+        case oneBlock
+        case costPerBlock
+        case inTheDetails
+        case pricing
+        case dareYou
+        case contact
         
-        static let allSections = [Installation, Settings, Links, Swab, WellVetted, OneBlock, CostPerBlock, InTheDetails, Pricing, DareYou, Contact]
+        static let allSections = [installation, settings, links, swab, wellVetted, oneBlock, costPerBlock, inTheDetails, pricing, dareYou, contact]
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if (UIScreen.mainScreen().bounds.size.width <= 320) {
+        if (UIScreen.main.bounds.size.width <= 320) {
             headerText!.font = UIFont(name: "Georgia", size: 24.0)
         } else {
             headerText!.font = UIFont(name: "Georgia", size: 28.0)
@@ -42,53 +42,53 @@ class SwabSettings: UITableViewController {
         self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return Section.allSections.count
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let section = Section(rawValue: section)!
         
         switch section {
-        case .Installation:
+        case .installation:
             return 1
-        case .Settings:
+        case .settings:
             return 1
-        case .Links:
+        case .links:
             return 4
-        case .Swab: fallthrough
-        case .WellVetted: fallthrough
-        case .OneBlock: fallthrough
-        case .InTheDetails: fallthrough
-        case .Pricing: fallthrough
-        case .DareYou: fallthrough
-        case .Contact: fallthrough
+        case .swab: fallthrough
+        case .wellVetted: fallthrough
+        case .oneBlock: fallthrough
+        case .inTheDetails: fallthrough
+        case .pricing: fallthrough
+        case .dareYou: fallthrough
+        case .contact: fallthrough
         default:
             return 1
         }
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let section = Section(rawValue: indexPath.section)!
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = Section(rawValue: (indexPath as NSIndexPath).section)!
         
         switch (section) {
-        case (.Installation):
-            let cell = tableView.dequeueReusableCellWithIdentifier("installation", forIndexPath: indexPath)
+        case (.installation):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "installation", for: indexPath)
             
             return cell
-        case (.Settings):
-            let cell = tableView.dequeueReusableCellWithIdentifier("swabSetting", forIndexPath: indexPath)
+        case (.settings):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "swabSetting", for: indexPath)
             
-            let switchControl = UISwitch(frame: CGRectZero)
-            switchControl.on = true
-            switchControl.enabled = false
+            let switchControl = UISwitch(frame: CGRect.zero)
+            switchControl.isOn = true
+            switchControl.isEnabled = false
             cell.accessoryView = switchControl
             
             return cell
-        case (.Links):
-            let cell = tableView.dequeueReusableCellWithIdentifier("link", forIndexPath: indexPath)
+        case (.links):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "link", for: indexPath)
             
-            switch indexPath.row {
+            switch (indexPath as NSIndexPath).row {
             case 0:
                 cell.textLabel?.text = "Website"
             case 1:
@@ -102,20 +102,20 @@ class SwabSettings: UITableViewController {
             }
             
             return cell
-        case (.Swab):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.swab):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "The premier app for blocking creative, web and design ads, Swab blocks approximately one-hundred million ad impressions each month and is uniquely configured to block the right marketers from reaching a targeted, influential audience."
             
             return cell
-        case (.WellVetted):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.wellVetted):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "We’re picky about the advertising we’ll block. We won’t block an ad unless it comes from the premier network for reaching creative, web and design professionals. Advertise something relevant to our audience and we’ll block your ad."
 
             return cell
-        case (.OneBlock):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.oneBlock):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "With the exception of “roadblocks,” which we’ll discuss later, there are only thirty-three advertising slots available to be blocked each month. Only a single ad will be blocked for each page viewed." +
                 "\n\n" +
@@ -124,28 +124,28 @@ class SwabSettings: UITableViewController {
                 "An ad blocked by Swab will not reach the creative community on the web in an uncluttered, controlled environment, far more valuable than a standard banner or a single text ad among dozens of others, because it was blocked. Current ads are also blocked on this page and not displayed here."
             
             return cell
-        case (.CostPerBlock):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.costPerBlock):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "We’re not blocking ads based on page views or hits or click-through, but if we were, the CPM for a block here would be priced well below industry norms. Most media who say “we don’t block on the numbers” do so because they don’t have the numbers to support their rates. That’s certainly not the case with Swab, as a group the apps block over one hundred million impressions each month. All that being said, it’s not about “cost-per-thousand,” it’s about “cost-per-block.”" +
             "\n\n" +
             "The loyal, regular blockers of the network’s ads and services consist of web publishers, writers, developers, editors, reporters and bloggers as well as influential designers and art directors. Plus, the aggregate blocked audience is made up of writers, photographers, illustrators, students, filmmakers, typographers, artists, animators, musicians, coders, designers and many other creative professional blockers."
 
             return cell
-        case (.InTheDetails):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.inTheDetails):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "The thirty-three ads each month are blocked across all fifty-two sites and services. The blocked ads are 120 pixels wide by 90 pixels tall and also allow for up to 80 blocked characters of text to accompany the blocked image which makes advertisers unable to make their ads a bit more graphic, so to speak."
             
             return cell
-        case (.Pricing):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.pricing):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "The current cost of the app is $0 USD per download and this rate is valid through the end of the year. Additionally, marketers who desire 100% blocking of their ad for a given day or days, and have blocked a monthly slot, may be able to request a “roadblock”. Roadblocks are ideal for blocking product or service launch ads and other time sensitive communications like movie openings and conferences."
             
             return cell
-        case (.DareYou):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.dareYou):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "Swab is built with Swift, and as we say “The best way to learn something new is to make something with it.”" +
                 "\n\n" +
@@ -154,8 +154,8 @@ class SwabSettings: UITableViewController {
                 "Swab was a fun app to build over the weekend. I’ll leave the ad-blocking hot takes to others."
             
             return cell
-        case (.Contact):
-            let cell = tableView.dequeueReusableCellWithIdentifier("bodyCopy", forIndexPath: indexPath)
+        case (.contact):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "bodyCopy", for: indexPath)
             
             cell.textLabel?.text = "For specific information, or to inquire about blocking an ad, please email Marco. Thank you."
             
@@ -163,64 +163,64 @@ class SwabSettings: UITableViewController {
         }
     }
     
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let section = Section(rawValue: section)!
         
         switch section {
-        case .Installation:
+        case .installation:
             return "INSTALLATION"
-        case .Settings:
+        case .settings:
             return "SETTINGS"
-        case .Links:
+        case .links:
             return "LINKS"
-        case .Swab:
+        case .swab:
             return "SWAB"
-        case .WellVetted:
+        case .wellVetted:
             return "WELL-VETTED"
-        case .OneBlock:
+        case .oneBlock:
             return "ONE BLOCK AT A TIME"
-        case .CostPerBlock:
+        case .costPerBlock:
             return "COST PER BLOCK"
-        case .InTheDetails:
+        case .inTheDetails:
             return "IN THE DETAILS"
-        case .Pricing:
+        case .pricing:
             return "PRICING AND ROADBLOCKS"
-        case .DareYou:
+        case .dareYou:
             return "I DARE YOU TO DEFEND IT"
-        case .Contact:
+        case .contact:
             return "CONTACT"
         }
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let section = Section(rawValue: indexPath.section)!
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let section = Section(rawValue: (indexPath as NSIndexPath).section)!
         
         switch (section) {
-        case (.Installation):
+        case (.installation):
             let title = "ENABLE VIA SETTINGS"
             var message = "Safari Content Blockers must be enabled via the Settings app"
             message += "\n\nSettings → Safari → Content Blockers"
             
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
             
-            let action = UIAlertAction(title: "I'll Enable Swab Manually", style: .Default, handler: nil)
+            let action = UIAlertAction(title: "I'll Enable Swab Manually", style: .default, handler: nil)
             alert.addAction(action)
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            self.present(alert, animated: true, completion: nil)
             
             // This URL will open the app's Settings page, not the Settings.app main page.
             // In iOS 8+ the app Settings page is installed automatically/randomly.
             //UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
             
-        case (.Settings):
+        case (.settings):
             
             // TODO: toggle Content Blocking via this switch?
             
             break
-        case (.Links):
+        case (.links):
             var urlString:String
             
-            switch indexPath.row {
+            switch (indexPath as NSIndexPath).row {
             case 0:
                 urlString = "http://swabthe.com"
             case 1:
@@ -234,39 +234,39 @@ class SwabSettings: UITableViewController {
                 break
             }
             
-            UIApplication.sharedApplication().openURL(NSURL(string: urlString)!)
+            UIApplication.shared.openURL(URL(string: urlString)!)
             
             break
-        case (.Swab): fallthrough
-        case (.WellVetted): fallthrough
-        case (.OneBlock): fallthrough
-        case (.InTheDetails): fallthrough
-        case (.Pricing): fallthrough
-        case (.DareYou): fallthrough
-        case (.Contact): fallthrough
+        case (.swab): fallthrough
+        case (.wellVetted): fallthrough
+        case (.oneBlock): fallthrough
+        case (.inTheDetails): fallthrough
+        case (.pricing): fallthrough
+        case (.dareYou): fallthrough
+        case (.contact): fallthrough
         default:
             break
         }
         
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         view.tintColor = UIColor(red: 247/255.0, green:247/255.0, blue:247/255.0, alpha:1)
 
         let headerView: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
         headerView.textLabel?.font = UIFont(name: "Verdana", size: 12.0)
     }
     
-    override func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+    override func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
         view.tintColor = UIColor(red: 247/255.0, green:247/255.0, blue:247/255.0, alpha:1)
     }
     
-    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 30.0
     }
     
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
     }
 
