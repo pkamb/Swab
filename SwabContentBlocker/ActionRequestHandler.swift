edeb613815b5bf66a66ff336fd11aec41b126b20
Peter Kamb <peterkamb@gmail.com>
2017-11-22 16:30:13 -0800

    refactor

--- SwabContentBlocker/ActionRequestHandler.swift
+++ SwabContentBlocker/ActionRequestHandler.swift
@@ -12,12 +12,15 @@ import MobileCoreServices
 class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
-        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
-    
-        let item = NSExtensionItem()
-        item.attachments = [attachment]
-    
-        context.completeRequest(returningItems: [item], completionHandler: nil);
+        var items = [NSExtensionItem]()
+        
+        if let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json")) {
+            let item = NSExtensionItem()
+            item.attachments = [attachment]
+            items.append(item)
+        }
+        
+        context.completeRequest(returningItems:items, completionHandler: nil);
     }
     
 }
